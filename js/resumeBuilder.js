2090e3440fa16a01f9d0b1c53991feff710a19d6
Phil Havlik <philhavlik@users.noreply.github.com>
2015-11-27 15:00:53 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,9 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var email = "phavlik78@hotmail.com";
+ 
+ var newEmail = email.replace("hotmail", "gmail");
+ 
+ console.log(email);
+ console.log(newEmail);
